3d3d91e0cbb2278b5b004e0ece429fb0aa35ece0
Emir Mehinbasic <emir96meh@gmail.com>
2026-07-01 23:02:44 +0200

    Installed winston logger

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import logger from './config/logger.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
+    logger:
   res.status(200).send('Aaaaaa');
 });
 
