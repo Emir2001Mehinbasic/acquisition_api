26455b020ddd4b4eda91da9462eec96ca6ada425
Emir Mehinbasic <emir96meh@gmail.com>
2026-07-03 23:59:43 +0200

    Added auth routs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import router from './routes/auth.routes.js';
+import { time } from 'drizzle-orm/mysql-core';
 
 const app = express();
 
@@ -22,5 +24,13 @@ app.get('/', (req, res) => {
   logger.info('Hello From acqusition');
   res.status(200).send('Aaaaaa');
 });
+
+app.get('/health', (req, res) => {
+  logger.info('Health Check');
+  res.status(200).json({ status: 'OK' , timestamp: new Date().toISOString() , uptime: process.uptime() });
+});
+
+
+app.use('/api/auth', authRoutes);
 
 export default app;
